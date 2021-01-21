c67db8b0931ed8bc1b1b34b44f15dd5c326c1d1e
Alaila <alaila2892@gmail.com>
2021-01-21 17:11:57 -0400

    load of folder python

---
+++ python/test/test.py
@@ -0,0 +1,16 @@
+print(1*2)
+print(2**4)
+print(7/4) 
+print(7%4) 
+a=5
+print(a)
+a=10
+print(a)
+print(a+a)
+a = a + a
+print(type(a))
+my_income = 100
+tax_rate = 0.1
+my_taxes = my_income * tax_rate
+print(my_taxes)
+
---
+++ python/test/test_2.py
@@ -0,0 +1,5 @@
+mystring = "Hello world"
+mystring[8]
+print(mystring[8])
+variable = "tinker"
+print(variable[1:4])
